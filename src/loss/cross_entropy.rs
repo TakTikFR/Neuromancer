b9c276e86469366240985bf1420d5f7f02bf13b8
Axel Segurens <segurens.axel@epita.fr>
2026-04-08 15:06:49 +0200

    Feat(Loss): Add the cross_entropy loss

---
+++ src/loss/cross_entropy.rs
@@ -0,0 +1,14 @@
+use crate::tensor::Tensor;
+use candle_core::Result;
+
+pub fn cross_entropy(probs: &Tensor, targets: &Tensor) -> Result<(Tensor, Tensor)> {
+    let loss = (targets * probs.log()?)?
+        .sum_keepdim(1)?
+        .mean_all()?
+        .neg()?;
+
+    let n = probs.dim(0)? as f64;
+    let grad = (targets / (probs * n)?)?.neg()?;
+
+    Ok((loss, grad))
+}
